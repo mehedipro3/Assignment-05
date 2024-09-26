47d1f793ed07c5e07c384a17f35428d997a55a53
mehedipro3 <mehediratul37@gmail.com>
2024-09-27 01:36:00 +0600

    all input are getting by function

--- js/home.js
+++ js/home.js
@@ -1,5 +1,5 @@
 
-            // noakhaliBalance task //
+            // Noakhali-Balance task //
 
 document.getElementById('noakhali-btn')
   .addEventListener('click', function() {
@@ -17,3 +17,43 @@ document.getElementById('noakhali-btn')
    const noakhaliNewBalance = noakhaliInput + noakhaliBalance ;
    document.getElementById('noakhali-balance').innerText = noakhaliNewBalance ;
   });
+
+            // Flood-Balance task //
+
+document.getElementById('flood-btn')
+  .addEventListener('click', function() {
+    const floodInput = getInputFieldById('flood-donate-input');
+    if (isNaN(floodInput)) {
+     alert('Failed to Donate');
+     return;
+   }
+   const balance = getTextFieldById('account-balance');
+   const floodBalance = getTextFieldById('flood-balance');
+   
+   const accountNewBalance = balance - floodInput ;
+   document.getElementById('account-balance').innerText = accountNewBalance ;
+   
+   const floodNewBalance = floodInput + floodBalance ;
+   document.getElementById('flood-balance').innerText = floodNewBalance ;
+  });
+
+
+  
+            // Quota-Balance task //
+
+document.getElementById('quota-btn')
+.addEventListener('click', function() {
+  const quotaInput = getInputFieldById('quota-donate-input');
+  if (isNaN(quotaInput)) {
+   alert('Failed to Donate');
+   return;
+ }
+ const balance = getTextFieldById('account-balance');
+ const quotaBalance = getTextFieldById('quota-balance');
+ 
+ const accountNewBalance = balance - quotaInput ;
+ document.getElementById('account-balance').innerText = accountNewBalance ;
+ 
+ const quotaNewBalance = quotaInput + quotaBalance ;
+ document.getElementById('quota-balance').innerText = quotaNewBalance ;
+});
