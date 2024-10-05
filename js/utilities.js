be2250730a5e2f33645cf95baa0bdd6ac1aba161
mehedipro3 <mehediratul37@gmail.com>
2024-10-05 22:19:12 +0600

    all done

--- js/utilities.js
+++ js/utilities.js
@@ -10,28 +10,6 @@ function getTextFieldById(id){
   return textNumber;
 }
 
-// function getModalBoxById(id){
-//   const showModal = noakhali-btn.showModal();
-//   //const showModal = document.createElement();
-
-
-//   showModal.innerHTML = `
-//   <dialog id="my_modal_1" class="modal">
-//               <div class="modal-box w-11/12 max-w-5xl">
-//                 <h3 class="text-lg font-bold">Hello!</h3>
-//                 <p class="py-4">Click the button below to close</p>
-//                 <div class="modal-action">
-//                   <form method="dialog">
-//                     <!-- if there is a button, it will close the modal -->
-//                     <button class="btn">Close</button>
-//                   </form>
-//                 </div>
-//               </div>
-//             </dialog>
-
-//       `       
-//  }
-
 
 function showSectionById(id){
   // hide all the sections
